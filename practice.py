a26df8fa40bb2469b8f9359edf43dfcc25e0c8b3
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-16 19:50:21 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,5 +1,14 @@
-print("hello")
-print("world")
-print("hello")
-print("world")
-print()
+def find_two_smallest(L):
+    """(see above) """
+    
+    #Find the index of the minimum and remove that item
+    smallest = min(L)
+    min1 = L.index(smalles)
+    L.remove(smallest)
+    #Find the index of the new minimum
+    next_smallest = min(L)
+    min2 = L.index(next_smallest)
+    
+# Put the smallest item back in the list 
+# If necessary, adjust the second index
+# Return the two indices
